981c5489b1870f0baa95f2ab70e56f3c85f4297d
raquel500c <raquel500c@gmail.com>
2017-09-19 13:43:51 +0200

    refactor app.js

--- app.js
+++ app.js
@@ -1,56 +1,15 @@
-const express = require('express')
-const path = require('path')
-const favicon = require('serve-favicon')
-const logger = require('morgan')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const expressLayout = require('express-ejs-layouts')
-const session = require("express-session")
-const flash = require("connect-flash")
-const passport = require('passport')
-const MongoStore = require("connect-mongo")(session)
-const debug = require('debug')("app:"+path.basename(__filename).split('.')[0])
-mongoose.Promise = global.Promise
-if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config()
-}
+const app = require('express')()
 
-mongoose.connect(process.env.MONGO_URI)
-  .then('Connected to DB')
-const app = express()
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.set('layout', 'layouts/main')
-app.use(expressLayout)
-app.use(session({
-  secret: "IronERP",
-  cookie: { maxAge: 60000 },
-  store: new MongoStore({
-    mongooseConnection: mongoose.connection,
-    ttl: 24 * 60 * 60  // 1 day
-  })
-}))
-app.use(flash())  
-app.locals.title = 'IronERP'
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
-require('./passport/serializers')
-require('./passport/local')
-app.use(passport.initialize())
-app.use(passport.session())
+require('./config/express')(app)
+
 const indexRoute = require('./routes/index')
 const authRoutes = require('./routes/auth')
 const dashboardRoute = require('./routes/dashboard')
+
 app.use(indexRoute)
 app.use(authRoutes)
 app.use(dashboardRoute)
+
 require('./config/error_404.js')
 require('./config/errors.js')
 
