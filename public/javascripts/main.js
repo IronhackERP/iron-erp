49040523b65ded357c7fa63d6b5df8582484fc41
raquel500c <raquel500c@gmail.com>
2017-09-24 00:05:21 +0200

    loaded user role selected in modal new user

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,45 +1,32 @@
 $(document).ready(function() {
-  Materialize.updateTextFields();
+  //Materialize.updateTextFields();
   $('select').material_select();
 
   $('#modal-suppliers-new').modal();
   $('#modal-users-new').modal();
+  $('#modal-products-new').modal();
+
   $('#modal-users-edit').modal();
 
-  $('.edit-button').click(function(e) {
+  $('.edit-button-user').click(function(e) {
     e.preventDefault()
-    const id = $(this).attr('data-user-id');
-    const username = $(this).parent().parent().children('.user-name').data('user-name')
-    const firstName = $(this).parent().parent().children('.first-name').data('first-name')
-    const lastName = $(this).parent().parent().children('.last-name').data('last-name')
-    const email = $(this).parent().parent().children('.user-email').data('user-email')
-    const rol = $(this).parent().parent().children('.user-rol').data('user-rol')
-
     const selectedUser = {
-      id: id,
-      username: username,
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      rol: rol
+      id: $(this).attr('data-user-id'),
+      username: $(this).parent().parent().children('.user-name').data('user-name'),
+      firstName: $(this).parent().parent().children('.first-name').data('first-name'),
+      lastName: $(this).parent().parent().children('.last-name').data('last-name'),
+      email: $(this).parent().parent().children('.user-email').data('user-email'),
+      rol: $(this).parent().parent().children('.user-rol').data('user-rol'),
     }
-    console.log("El objeto a editar es: ");
-    console.log(selectedUser);
-    console.log(selectedUser.rol);
-
-    setDataFormModal(selectedUser);
+    setDataUserFormModal(selectedUser);
   })
 });
-//$('#rol-modal-edit> option[value="admin"]').attr('selected', 'selected');
-function setDataFormModal(selectedUser) {
+
+function setDataUserFormModal(selectedUser) {
   $('#user-modal-edit').val(selectedUser.username);
   $('#first-name-modal-edit').val(selectedUser.firstName);
   $('#last-name-modal-edit').val(selectedUser.lastName);
   $('#email-modal-edit').val(selectedUser.email);
-  if ((selectedUser.rol) === 'admin') {
-    $('#rol-modal-edit> option[value="admin"]').attr('selected', 'selected');
-  }
-  if ((selectedUser.rol) === 'employee') {
-    $('#rol-modal-edit> option[value="employee"]').attr('selected', 'selected');
-  }
+  $(`select#rol-modal-edit-user > option[value=${selectedUser.rol}]`).attr('selected','selected');
+  $('select#rol-modal-edit-user').material_select();
 }
