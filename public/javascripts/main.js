d3fae10088d65ec57ca12a8cd7ada2e3b0b9dbca
raquel500c <raquel500c@gmail.com>
2017-09-24 18:04:15 +0200

    show suppliers phoneNumber ok

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -19,17 +19,40 @@ $(document).ready(function() {
     }
     setDataUserFormEdit(selectedUser);
   })
+
+  $('.edit-button-user').click(function(e) {
+    e.preventDefault()
+    const selectedUser = {
+      id: $(this).attr('data-user-id'),
+      username: $(this).parent().parent().children('.user-name').data('user-name'),
+      firstName: $(this).parent().parent().children('.first-name').data('first-name'),
+      lastName: $(this).parent().parent().children('.last-name').data('last-name'),
+      email: $(this).parent().parent().children('.user-email').data('user-email'),
+      rol: $(this).parent().parent().children('.user-rol').data('user-rol'),
+    }
+    setDataUserFormEdit(selectedUser);
+  })
 });
 
   //CORRECTO--->
 function setDataUserFormEdit(selectedUser) {
-  $('#title-modal-edit').text(`Edit User ${selectedUser.username}`);
-  $('#id-modal-edit').attr('action',`/users/${selectedUser.id}/edit`);
+  $('#title-modal-edit-user').text(`Edit User ${selectedUser.username}`);
+  $('#id-modal-edit-user').attr('action',`/users/${selectedUser.id}/edit`);
   $('#user-modal-edit').val(selectedUser.username);
   $('#first-name-modal-edit').val(selectedUser.firstName);
   $('#last-name-modal-edit').val(selectedUser.lastName);
-  $('#email-modal-edit').val(selectedUser.email);
+  $('#email-modal-edit-user').val(selectedUser.email);
   $(`select#rol-modal-edit-user > option[value=${selectedUser.rol}]`).attr('selected', 'selected');
   $('select#rol-modal-edit-user').material_select();
+  Materialize.updateTextFields();
+}
+
+function setDataSupplierFormEdit(selectedSupplier) {
+  $('#title-modal-edit-supplier').text(`Edit Supplier ${selectedSupplier.name}`);
+  $('#id-modal-edit-supplier').attr('action',`/users/${selectedSupplier.id}/edit`);
+  $('#supplier-modal-edit').val(selectedSupplier.name);
+  $('#company-name-modal-edit').val(selectedSupplier.companyName);
+  $('#phone-number-modal-edit').val(selectedSupplier.phoneNumber);
+  $('#email-modal-edit').val(selectedSupplier.email);
   Materialize.updateTextFields();
 }
