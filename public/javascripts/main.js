3663e93adc7ebc32a5c53c72ec7358d55d256285
raquel500c <raquel500c@gmail.com>
2017-09-24 22:34:59 +0200

    user, supplier & client styles

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,29 +1,15 @@
 $(document).ready(function() {
-
   $('.product-client-list').select2();
 
   Materialize.updateTextFields();
 
   $('select').material_select();
   $('#modal-suppliers-new').modal();
-    $('#modal-suppliers-edit').modal();
+    //$('#modal-suppliers-edit').modal();
   $('#modal-users-new').modal();
   $('#modal-users-edit').modal();
 
   //CORRECTO--->
-  $('.edit-button-user').click(function(e) {
-    e.preventDefault()
-    const selectedUser = {
-      id: $(this).attr('data-user-id'),
-      username: $(this).parent().parent().children('.user-name').data('user-name'),
-      firstName: $(this).parent().parent().children('.first-name').data('first-name'),
-      lastName: $(this).parent().parent().children('.last-name').data('last-name'),
-      email: $(this).parent().parent().children('.user-email').data('user-email'),
-      rol: $(this).parent().parent().children('.user-rol').data('user-rol'),
-    }
-    setDataUserFormEdit(selectedUser);
-  })
-
   $('.edit-button-user').click(function(e) {
     e.preventDefault()
     const selectedUser = {
@@ -48,15 +34,5 @@ function setDataUserFormEdit(selectedUser) {
   $('#email-modal-edit-user').val(selectedUser.email);
   $(`select#rol-modal-edit-user > option[value=${selectedUser.rol}]`).attr('selected', 'selected');
   $('select#rol-modal-edit-user').material_select();
-  Materialize.updateTextFields();
-}
-
-function setDataSupplierFormEdit(selectedSupplier) {
-  $('#title-modal-edit-supplier').text(`Edit Supplier ${selectedSupplier.name}`);
-  $('#id-modal-edit-supplier').attr('action',`/users/${selectedSupplier.id}/edit`);
-  $('#supplier-modal-edit').val(selectedSupplier.name);
-  $('#company-name-modal-edit').val(selectedSupplier.companyName);
-  $('#phone-number-modal-edit').val(selectedSupplier.phoneNumber);
-  $('#email-modal-edit').val(selectedSupplier.email);
   Materialize.updateTextFields();
 }
